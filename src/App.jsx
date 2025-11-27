6a7cafb8e5becbe7cc99b0bc2e399ba1c9eba02c
Ladra Bastien <ladra.bastien@gmail.com>
2025-11-27 07:40:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CVCyber from "./pages/CVCyber";
 
